e359d4eaff9b124daab24a021e02d09dc40b2d39
packtsurabhi <33089349+packtsurabhi@users.noreply.github.com>
2018-03-29 17:46:37 +0530

    Add files via upload

---
+++ chapter7/catalog-service-openshift-postgresql/src/main/java/org/packt/swarm/petstore/catalog/CatalogService.java
@@ -0,0 +1,33 @@
+package org.packt.swarm.petstore.catalog;
+
+import org.packt.swarm.petstore.catalog.model.Item;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+
+
+@ApplicationScoped
+public class CatalogService {
+
+    @PersistenceContext(unitName = "CatalogPU")
+    private EntityManager em;
+
+    public Item searchById(String itemId) {
+        return em.createNamedQuery("Item.findById", Item.class).setParameter("itemId", itemId).getResultList().get(0);
+    }
+
+    public List<Item> getAll() {
+        return em.createNamedQuery("Item.findAll", Item.class).getResultList();
+    }
+
+    @Transactional
+    public void add(Item item){
+        item.setItemId(UUID.randomUUID().toString());
+        em.persist(item);
+    }
+
+}
